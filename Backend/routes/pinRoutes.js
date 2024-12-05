0f9536db96b029cf2733139ab48b457992c639a6
Chirag Soni <csoni0693@gmail.com>
2024-12-05 11:33:54 +0530

    pinterest

--- Backend/routes/pinRoutes.js
+++ Backend/routes/pinRoutes.js
@@ -1,5 +1,7 @@
 import express from 'express';
-
+import { isAuth } from '../middlewares/isAuth.js';
+import uploadFile from '../middlewares/multer.js';
+import { createPin } from '../controllers/pinController.js';
 const router = express.Router();
-
+router.post("/new",isAuth,uploadFile,createPin);
 export default router;
