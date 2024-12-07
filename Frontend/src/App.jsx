4ef25f6bc331ce23b37afdccfa220be257c885be
Chirag Soni <csoni0693@gmail.com>
2024-12-07 07:47:34 +0530

    pinterest

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,28 +5,26 @@ import Register from "./pages/Register";
 import { UserData } from "./context/UserContext";
 import { Loading } from "./components/Loading";
 import Navbar from "./components/Navbar";
+import Pinpage from "./pages/PinPage";
 
 function App() {
   const { loading, isAuth, user } = UserData();
   return (
     <>
       {loading ? <Loading /> : <BrowserRouter>
-        {isAuth && <Navbar user={user}/>}
-          <Routes>
-            <Route
-              path="/"
-              element={isAuth ? <Home /> : <Login />}
-            />
-            <Route
-              path="/login"
-              element={isAuth ? <Home /> : <Login />}
-            />
-            <Route
-              path="/register"
-              element={<Register />}
-            />
-          </Routes>
-        </BrowserRouter>}
+        {isAuth && <Navbar user={user} />}
+        <Routes>
+          <Route
+            path="/"
+            element={isAuth ? <Home /> : <Login />}
+          />
+          <Route path="/login" element={isAuth ? <Home /> : <Login />}
+          />
+          <Route path="/register" element={<Register />}
+          />
+          <Route path="/pin/:id" element={isAuth ? <Pinpage user={user} /> : <Login />}></Route>
+        </Routes>
+      </BrowserRouter>}
     </>
 
   );
